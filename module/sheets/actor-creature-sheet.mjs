576c29cec4df43088fa32e0d64ecda29e80ae8f2
Szymon Gosek <quadur@gmail.com>
2026-01-06 11:20:24 +0100

    A check for determining the number of creature attacks before
    Added a check for available attacks before rolling and using the current number independent from the standard six from the base books.

--- module/sheets/actor-creature-sheet.mjs
+++ module/sheets/actor-creature-sheet.mjs
@@ -53,12 +53,15 @@ export class cgdActorCreatureSheet extends cgdActorNpcSheet {
 
   static async _attack(event, target) {
     const attacks = this.actor.items.filter(it => it.type == "creatureAttack");
-    const roll = await new Roll("1d6").roll();
+    if (!(attacks?.length > 0))
+      console.log("The creature does not have any attacks!");
+      return this.actor;
+    const roll = await new Roll(`1d${attacks.length}`).roll();
     let attackNumber = roll.total;
     console.log(attackNumber, this.actor.system.lastAttack);
     if (this.actor.system.lastAttack == attackNumber)
       attackNumber += 1;
-    if (attackNumber == 7)
+    if (attackNumber == attacks.length)
       attackNumber = 1;
     const currentAttack = attacks[attackNumber - 1];
     await currentAttack.automate(event);
